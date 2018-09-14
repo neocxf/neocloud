1f1e38b6f3862cb1973383bbf38c0a4e3189a533
neo.chen <neocxf@gmail.com>
2018-09-14 13:55:26 +0800

    add catalog

--- gateway/src/main/java/top/neospot/cloud/gateway/GatewayEndpoint.java
+++ gateway/src/main/java/top/neospot/cloud/gateway/GatewayEndpoint.java
@@ -6,9 +6,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 /**
  * By neo.chen{neocxf@gmail.com} on 2018/8/24.
@@ -27,6 +31,21 @@ public class GatewayEndpoint {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("top.neospot.cloud"))
-                .build();
+                .build()
+                .apiInfo(metaData())
+                ;
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfo(
+                "Spring Boot REST API",
+                "Spring Boot REST API for Online Store",
+                "1.0",
+                "Terms of service",
+                new Contact("John Thompson", "https://springframework.guru/about/", "john@springfrmework.guru"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                Collections.emptyList()
+        );
     }
 }
