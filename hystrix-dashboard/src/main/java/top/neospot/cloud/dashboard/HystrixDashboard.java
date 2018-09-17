c5c080684708ad0dcbdea911b669d10c74b41d9d
neo.chen <neocxf@gmail.com>
2018-09-17 08:43:48 +0800

    add dashboard

--- hystrix-dashboard/src/main/java/top/neospot/cloud/dashboard/HystrixDashboard.java
+++ hystrix-dashboard/src/main/java/top/neospot/cloud/dashboard/HystrixDashboard.java
@@ -2,12 +2,14 @@ package top.neospot.cloud.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableHystrixDashboard
-@EnableTurbine
+//@EnableTurbineStream
+
 public class HystrixDashboard {
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboard.class, args);
